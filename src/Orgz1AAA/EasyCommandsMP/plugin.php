a32094ea5d1cfd5ff8723adc73f2ca62454cd76b
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-05-04 15:29:59 -0500

    Update plugin.php

--- src/Orgz1AAA/EasyCommandsMP/plugin.php
+++ src/Orgz1AAA/EasyCommandsMP/plugin.php
@@ -4,4 +4,12 @@ namespace Orgz1AAA\EasyCommandsMP;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+
+class EasyCommandsMP extends PluginBase{
+  if(strtolower($command->getName()) === "create"){
+    
+    return true;
+  }
+  return false;
+}
 ?>
