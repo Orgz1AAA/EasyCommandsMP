996543c37e6c6a576431c7f59a99a765739415eb
Orgz1AAA <dthomassmith@marshalltown.k12.ia.us>
2017-05-05 14:10:07 -0500

    Update plugin.php

--- src/Orgz1AAA/EasyCommandsMP/plugin.php
+++ src/Orgz1AAA/EasyCommandsMP/plugin.php
@@ -4,6 +4,8 @@ namespace Orgz1AAA\EasyCommandsMP;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 
 class EasyCommandsMP extends PluginBase{
   if(strtolower($command->getName()) === "create"){
